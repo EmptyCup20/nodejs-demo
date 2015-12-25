aec94bbff7f0e2c978b2baa1ba0e980cfcafe415
xiangxiao3 <xxholly32@163.com>
2015-12-25 15:19:38 +0800

    add express-demo

--- demo/express-demo/app.js
+++ demo/express-demo/app.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express')
+    , routes = require('./routes')
+    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , logger = require('morgan')
+    , http = require('http')
+    , path = require('path')
+    , ejs = require('ejs');
 
 var app = express();
 
-// view engine setup
+//设置默认端口, 启动views
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');// app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,38 +23,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
+var server = http.createServer(app),
+    io = require("socket.io").listen(server);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function tick(){
+    var now = new Date().toUTCString();
+    io.sockets.send(now);
+    console.log(now)
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+setInterval(tick, 1000);
+
+server.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-module.exports = app;
--- demo/express-demo/routes/firstsocket.js
+++ demo/express-demo/routes/firstsocket.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 /* GET firstsocket page. */
 router.get('/firstsocket', function(req, res, next) {
-  res.render('firstsocket', null);
+    res.render('firstsocket', null);
 });
 
 module.exports = router;

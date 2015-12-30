bee37836efb684ecb404b23e3104c5cfff82a792
xiangxiao3 <xxholly32@163.com>
2015-12-30 15:49:35 +0800

    add
    whole nodejs+express+mongodb+angularjs demo

---
+++ demo/express-demo/public/javascripts/application.js
@@ -0,0 +1,138 @@
+/**
+ *
+ * @Author zhangxin14
+ * @Date   2015/11/23
+ *
+ */
+var app = angular.module("myApp", []).controller("EmployeeCtrl", ["$scope", "$http", function ($scope, $http) {
+    $scope.editing = false;
+
+    //获取用户,用jquery的ajax会导致scope不变化的问题出现
+    var getUsers = function(){
+        $http.post("./getUsers").success(function(data) {
+            $scope.employees = data;
+        });
+    }
+    getUsers();
+
+
+    $scope.selectedEmployee = null;
+    $scope.editingEmployee = {};
+
+    $scope.provinceArr = ["江苏", "云南"];
+    $scope.cityArr = [];
+    $scope.countyArr = [];
+
+    $scope.formatGender = function (gender) {
+        if (gender == 0)
+            return "女";
+
+        if (gender == 1)
+            return "男";
+    };
+
+    $scope.$watch("selectedEmployee", function (employee) {
+        $scope.editingEmployee = employee || {};
+    });
+
+    $scope.$watch("editingEmployee.province", function (province) {
+        // 真正有用的代码在这里，实际场景中这里可以是调用后端服务查询的关联数据
+        switch (province) {
+            case "江苏":
+            {
+                $scope.cityArr = ["南京", "苏州"];
+                break;
+            }
+            case "云南":
+            {
+                $scope.cityArr = ["昆明", "丽江"];
+                break;
+            }
+        }
+    });
+
+    $scope.selectEmployee = function (employee) {
+        $scope.selectedEmployee = employee;
+    };
+
+    $scope.create = function () {
+        $scope.state = "New";
+        $scope.editing = true;
+
+        $scope.editingEmployee = {};
+    };
+
+    $scope.modify = function () {
+        $scope.state = "Modify";
+        $scope.editing = true;
+
+        $scope.editingEmployee = angular.extend({}, $scope.selectedEmployee);
+    };
+
+    $scope.remove = function () {
+        if (confirm("确认删除此员工吗？")) {
+            //保存用户
+            $http({
+                method : "POST",
+                url : "/removeUser",
+                params : {
+                    "_id" : $scope.selectedEmployee._id
+                }
+            }).success(function(data){
+                for (var i = 0; i < $scope.employees.length; i++) {
+                    if ($scope.employees[i] == $scope.selectedEmployee) {
+                        $scope.employees.splice(i, 1);
+                        $scope.selectedEmployee = null;
+                        break;
+                    }
+                }
+            })
+        }
+    };
+
+    $scope.ok = function () {
+        //保存用户
+        $http({
+            method : "POST",
+            url : "/saveUser",
+            params : {
+                "_id" : $scope.editingEmployee._id,
+                "name" : $scope.editingEmployee.name,
+                "age" : $scope.editingEmployee.age,
+                "gender" : $scope.editingEmployee.gender,
+                "province" : $scope.editingEmployee.province,
+                "city" : $scope.editingEmployee.city,
+                "minority" : $scope.editingEmployee.minority
+            }
+        }).success(function(data){
+            //保存后的数据返回
+            if ($scope.state === "New") {
+                $scope.employees.push(data);
+                $scope.selectedEmployee = data;
+            }
+            else if ($scope.state === "Modify") {
+                $scope.selectedEmployee = angular.extend({}, $scope.editingEmployee, data);
+            }
+            $scope.state = "View";
+            $scope.editing = false;
+
+            getUsers();
+        })
+    };
+
+    $scope.cancel = function () {
+        $scope.state = "View";
+        $scope.editing = false;
+
+        $scope.editingEmployee = $scope.selectedEmployee;
+    };
+
+    $scope.okDisabled = function () {
+        if ($scope.editing && ($scope.editingEmployee.name) && ($scope.editingEmployee.name.length >= 0)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
+}]);
--- demo/express-demo/routes/index.js
+++ demo/express-demo/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     router = express.Router(),
-    http = require('http');
+    http = require('http'),
+    User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +16,28 @@ router.get('/firstsocket', function(req, res, next) {
 /* GET firstsocket page. */
 router.get('/firsthttp', function(req, res, next) {
   res.render('firsthttp', null);
+});
+
+/* 模拟一个post 方法 保存用户 xx */
+router.post('/saveUser', function(req, res) {
+    var user = new User(req.query);
+    user.save(function(message, data){
+        res.send(data);
+    });
+});
+
+/* 模拟一个post 方法 获取所有用户 xx */
+router.post('/getUsers', function(req, res) {
+    User.get(null, function(message, data){
+        res.send(data);
+    });
+});
+
+/* 模拟一个post 方法 删除用户 xx */
+router.post('/removeUser', function(req, res) {
+    User.remove(req.query._id, function(message, data){
+        res.send(data);
+    });
 });
 
 module.exports = router;
